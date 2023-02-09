3f9f41aac07fb02d5b9a868a11cdf36b2d3faba3
Tim Jentzsch <tim-jentzsch@gmx.de>
2023-02-09 22:24:48 +0100

    Print scan codes of pressed keys

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_system(print_scan_code)
+        .run();
+}
+
+fn print_scan_code(scan_code_input: Res<Input<ScanCode>>) {
+    for scan_code in scan_code_input.get_just_pressed() {
+        println!("Pressed scan code 0x{:x}", scan_code.0);
+    }
 }
